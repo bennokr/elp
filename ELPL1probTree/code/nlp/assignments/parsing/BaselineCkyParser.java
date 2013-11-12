07f4c943685a3c595fc497e411166506bfee3c66
bennokr <bennokr@gmail.com>
2013-11-12 11:34:09 +0100

    dingen testen

--- ELPL1probTree/code/nlp/assignments/parsing/BaselineCkyParser.java
+++ ELPL1probTree/code/nlp/assignments/parsing/BaselineCkyParser.java
@@ -259,7 +259,6 @@ class BaselineCkyParser implements Parser {
 		double logScore = 0;
 
 		if (annotatedTree.isLeaf()){
-			System.out.println("A leaf has no log score");
 			return 0; //something
 		}
 
