c68213f85d41bd796eae5b624a6a4a440562cd5f
Veldhoen <sara.veldhoen+github@gmail.com>
2013-11-21 09:35:33 +0100

    to old project and bug fix

---
+++ ELPL1probTree/codeAss2/nlp/assignments/parsing/BinaryRule.java
@@ -0,0 +1,25 @@
+package nlp.assignments.parsing;
+
+public class BinaryRule extends Rule {
+
+	public BinaryRule(String parent, String[] children, boolean annotated) {
+		super(parent, children, annotated);
+		// TODO Auto-generated constructor stub
+	}
+	
+	  public BinaryRule(String parent, String leftChild, String rightChild) {
+		  super(parent, new String[0],false);
+		  String[] children = {leftChild,rightChild};
+	      super.setChildren(children);
+	  }
+	  
+	  public String getLeftChild() {
+	      return super.getChildren()[0];
+	    }
+
+	    public String getRightChild() {
+	      return super.getChildren()[1];
+	    }
+
+
+}
